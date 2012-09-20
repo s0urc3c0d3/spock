05a6eb71fcee738f774de5e3962f48ef07cc376f
Grzegorz Dwornicki <g.r.d0110@gmail.com>
2012-09-20 10:53:39 +0200

    Added: - partition chosse dialog - yes/no for manual /boot location - radio for manual /boot on specific partition - radio for grub version
    TODO:
    - chosse Virtualization server
    - chosse login method
    - get login credentials
    - repartition physical partitions when building virtual hard drive
    - chosse type of virtual hard drive
    - chosse name of VM
    - chosse location of virual hard drive
    - chosse network settings (many dialogs will be needed)
    - auto start machine dialog

--- spock.sh
+++ spock.sh
@@ -5,8 +5,24 @@
 #ustalenie jakie dyski i partycje beda przenoszone na VM
 
 tmp=$(fdisk -l | grep '^/' | sed 's/\*//' | awk '{print $1"-"$2"-"$3"-"$4"-"$5"-"$6 } ')
+cmd=""
 for i in $tmp; do
-	echo $i | awk 'BEGIN {FS="-"} {print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6 }'
+	item=$(echo $i | awk 'BEGIN {FS="-"} {print $6 }')
+	tag=$(echo $i | awk 'BEGIN {FS="-"} {print $1}')
+	cmd="$cmd $tag ${item} 0" 
 done
-echo $tmp
-#dialog --backtitle "SPOCK v1.0" --title Partitions --checklist "Chosse partitions to migrate" 10 60
+#dialog --backtitle "SPOCK v1.0" --title Partitions --checklist "Chosse partitions to migrate" 10 60 10 $cmd 2> /tmp/partitions
+
+#dialog --backtitle "SPOCK v1.0" --title "MBR Grub" --yesno "Do you wish to select manualy the partition with /boot?" 5 60
+
+#dialog --backtitle "SPOCK v1.0" --title "MBR Grub" --radiolist "Chosse bootloader" 10 60 5 grub "version 1" 0 grub2 "vesion 2" 0
+
+cmd=""
+for i in `cat /tmp/partitions | sed 's/"//g'`; do
+	item=$i
+	tag=$i
+	cmd="$cmd $tag $item 0"
+done
+
+#dialog --backtitle "SPOCK v1.0" --title "/boot location" --radiolist "Chosse partition with /boot" 10 60 5 $cmd
+
