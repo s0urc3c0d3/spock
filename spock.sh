7cceb1f5ccdc1bd84af9e2a58280269c788d21f4
Grzegorz Dwornicki <g.r.d0110@gmail.com>
2012-09-27 22:15:48 +0200

    fixes to spock.sh, added live-helper tree

--- spock.sh
+++ spock.sh
@@ -82,7 +82,7 @@ do
 	echo $password | sshfs ${username}@${servername}: /mnt/home -o password_stdin -o CheckHostIP="no" -o uid=0 -o gid=0 -o umask=7077
 
 	ssh-keygen -t rsa -b 4096 -f ~/.ssh/spock -N ""
-	if ! [ -d /mnt/home/.ssh ]; then mkdir /mnt/home/.ssh; chmod 0600 /mnt/home/.ssh; fi
+	if ! [ -d /mnt/home/.ssh ]; then mkdir /mnt/home/.ssh; chmod 0700 /mnt/home/.ssh; fi
 	cp ~/.ssh/spock.pub /mnt/home/.ssh/
 	if ! [ -f /mnt/home/.ssh/authorized_keys ]; then touch /mnt/home/.ssh/authorized_keys ; fi
 	cp /mnt/home/.ssh/authorized_keys /mnt/home/.ssh/authorized_keys_old
